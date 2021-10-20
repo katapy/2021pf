2183d1837da58f1ee4911589f5f9608c352b4cbd
katapy <katapy.tennis@gmail.com>
2021-10-20 22:28:16 +0900

    Edit ssh send setting.

--- command/ssh_linux.sh
+++ command/ssh_linux.sh
@@ -13,7 +13,7 @@ KEYPUB="./mac/authorized_keys"
 
 # 古いフォルダの削除
 echo "delete old file."
-ssh katapy@$HOSTNAME "cd mac;rm -rf *;"
+ssh katapy@$HOSTNAME "chmod -R 755 ~/mac; cd mac; rm -rf *;"
 
 # ファイル転送
 echo "send file."
